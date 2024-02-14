1c109c79876fe1be0977cc3e76ac220f3c750af1
Nicolas Andres Kurek <131825518+NinAndres@users.noreply.github.com>
2024-02-14 12:23:49 -0300

    project created

---
+++ src/main/java/com/ninandres/workmongodb/WorkmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.ninandres.workmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ninandres/workmongodb/WorkmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninandres.workmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
